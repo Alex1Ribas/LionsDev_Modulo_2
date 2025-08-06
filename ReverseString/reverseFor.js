e562022866b0ae4003b171e3e4cd5ee3219f451d
Alex <alex.ribas12@gmail.com>
2025-08-06 11:50:37 -0300

    atualização

---
+++ ReverseString/reverseFor.js
@@ -0,0 +1,10 @@
+// reverse por laço de repetição
+function ReverseString(str){
+    let reversed = "";
+    for (let i = 0; str.length - 1; i >=0, i--){
+        reverserd += str[i]
+    }
+    return reversed
+}
+
+console.log("");
